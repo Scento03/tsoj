b4054c1e229403868068c9a720d9ae724f4c5f41
Scento03 <dlsdud772@gmail.com>
2021-01-09 06:21:30 +0900

    2장 2문제 추가

--- tsoj/src/tsoj/ch01/Ex01.java
+++ tsoj/src/tsoj/ch01/Ex01.java
@@ -3,7 +3,10 @@ package tsoj.ch01;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world????");
+		System.out.println("Hello, world."); //
+
+		
+		
 
 	}
 
